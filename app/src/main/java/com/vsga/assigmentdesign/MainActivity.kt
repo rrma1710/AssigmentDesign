fc5104affaaed0dccea3184dd37edf09d59e67d9
rrma1710 <rrma1710@gmail.com>
2023-07-02 16:38:15 +0700

    Update Login

--- app/src/main/java/com/vsga/assigmentdesign/MainActivity.kt
+++ app/src/main/java/com/vsga/assigmentdesign/MainActivity.kt
@@ -7,7 +7,7 @@ import android.view.MotionEvent
 import android.view.View
 import com.vsga.assigmentdesign.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +16,26 @@ class MainActivity : AppCompatActivity(){
 
 
         binding.btnMasuk.setOnClickListener {
-            val intent = Intent(this, DashboardActivity::class.java)
-            startActivity(intent)
+
+            val username = binding.edtUsername.text.toString()
+            val password = binding.edtPassword.text.toString()
+
+            if (username.isEmpty()) {
+                binding.edtUsername.error = "Username tidak boleh kosong"
+                binding.edtUsername.requestFocus()
+                return@setOnClickListener
+            } else if (password.isEmpty()) {
+                binding.edtPassword.error = "Password tidak boleh kosong"
+                binding.edtPassword.requestFocus()
+                return@setOnClickListener
+            } else if (username.equals("admin",true) && password == "admin123") {
+                val intent = Intent(this, DashboardActivity::class.java)
+                startActivity(intent)
+            } else {
+                binding.edtUsername.error = "Username atau password salah"
+                binding.edtUsername.requestFocus()
+                return@setOnClickListener
+            }
         }
 
         binding.tvDaftar.setOnClickListener {
